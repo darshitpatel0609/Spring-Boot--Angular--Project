79a7cb780dc3b52c83b663bc37bea86152630ab3
Darshit Patel <darashitpatel4964@gmail.com>
2023-04-24 12:32:13 +0530

    Angular Practical

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:SignupComponent,path:"signup"
+  },
+  {
+    component:LoginComponent,path:"login"
+  },
+  {
+    component:ForgetPasswordComponent,path:"forget-password"
+  },
+  {   
+    path:"",component:LoginComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
